c333a1aa1feac3adf556c8013b1f88b2deb6780d
Sophiss1403 <sophiss.1403@gmail.com>
2020-12-26 16:49:49 -0300

    Update Exercicio103.py
    Funciona, mas não tá bem escrita

--- Exercicio103.py
+++ Exercicio103.py
@@ -1,14 +1,16 @@
 def separador():
     return '-' * 30
 
+
 def ficha(nome='<desconhecido>', gols=0):
     nome = input('Nome: ')
     if not nome:
-        nome='<desconhecido>'
+        nome = '<desconhecido>'
     gols = input('Gols: ')
     if not gols:
         gols = int(0)
     return f'O jogador {nome} fez {gols} gol(s).'
+
 
 print(separador())
 print(ficha())
