972180d5800b344bb4991259fca52285b421f7ff
Sophiss1403 <sophiss.1403@gmail.com>
2020-12-26 19:52:27 -0300

    Correção professor

--- Exercicio103.py
+++ Exercicio103.py
@@ -1,8 +1,15 @@
 '''Esse exercício é sobre parâmetros opcionais em funções
 
-Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e quantos gols ele marcou.
-O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente. 
+Faça um programa que tenha uma função chamada ficha(),
+que receba dois parâmetros opcionais: o nome de um jogador
+e quantos gols ele marcou. O programa deverá ser capaz de
+mostrar a ficha do jogador, mesmo que algum dado não tenha
+sido informado corretamente.
 '''
+
+
+def ficha(nome='<desconhecido>', gols=''):
+    print(f'O jogador {nome} fez {gols} gol(s).')
 
 
 # Programa principal
@@ -12,7 +19,8 @@ if g.isnumeric():   # .isdecimal é melhor pq isnumeric cria um bug
     g = int(g)
 else:
     g = 0
-if n.strip() == '':  # muito boa a escolha pelo strip, já trata a possiblidade da pessoa digitar vários espaços
+if n.strip() == '':  # muito boa a escolha pelo strip, já trata a possiblidade
+    # da pessoa digitar vários espaços
     ficha(gol=g)
 else:
     ficha(n, g)
